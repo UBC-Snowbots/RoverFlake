aa5f7b645a62dbda49568939f7b505c610de2723
Roozki <zawadzkirowan@gmail.com>
2023-07-15 10:12:07 -0700

    needs testing

---
+++ src/mcu_serial_comm_drivers/include/stmUtilityComm.h
@@ -0,0 +1,67 @@
+/*
+ * Created By: Tate Kolton and Ihsan Olawale
+ * Created On: July 4, 2022
+ * Description: Header file for recieving messages from pro controller and
+ * relaying them to arm hardware driver module
+ */
+
+#ifndef ARM_HARDWARE_DRIVER_MYNODE_H
+#define ARM_HARDWARE_DRIVER_MYNODE_H
+
+// STD Includes
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <unistd.h>
+
+// ROS Includes
+#include <math.h>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <vector>
+
+// Snowbots Includes
+#include <sb_msgs/ArmPosition.h>
+// #include <sb_utils.h> //from snowflake
+
+// Other
+#include <serial/serial.h>
+
+
+using std::string;
+using std::exception;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+
+class STMComm {
+  public:
+    STMComm(ros::NodeHandle& nh);
+    void sendMsg(std::string outMsg);
+    void recieveMsg();
+    void pubUtility();
+    
+    //new serial
+    unsigned long baud = 9600;
+    string port = "/dev/ttyACM1"; //will be best if we sift through
+
+
+
+    // character representations of buttons for arm communication
+    const char T1 = '1';
+    const char T2 = '2';
+    // const char J3 = '3';
+    // const char J4 = '4';
+    // const char J5 = '5';
+    // const char J6 = '6';
+
+  private:
+    ros::NodeHandle nh;
+
+  
+    ros::Publisher pubUtilityData;
+
+    serial::Serial stm_board;
+};
+#endif // ARM_HARDWARE_DRIVER_MYNODE_H
---
+++ src/mcu_serial_comm_drivers/src/stmUtilityComm.cpp
@@ -0,0 +1,75 @@
+/*
+ * Created By: Tate Kolton
+ * Created On: July 4, 2022
+ * Description: This package receives info from /cmd_arm topic and publishes
+ * serial data via callback to be recieved by the arm MCU (Teensy 4.1)
+ */
+
+#include "../include/stmUtilityComm.h"
+
+STMComm::STMComm(ros::NodeHandle& nh) : nh(nh) {
+    // Setup NodeHandles
+
+    ros::NodeHandle private_nh("~");
+
+    // Setup Subscribers
+    int queue_size = 55;
+
+   // subPro = nh.subscribe( 
+   //     "/cmd_arm", queue_size, &ArmHardwareDriver::allControllerCallback, this);
+
+   pubUtilityData = private_nh.advertise<std_msgs::String>("/utility_board_data", 1);
+
+
+    stm_board.setBaudrate(baud);
+    stm_board.setPort(port);
+    stm_board.open();
+    stm_board.setDTR(false);
+    stm_board.setRTS(false);
+
+    while(ros::ok()){
+        if(stm_board.available() > 0){
+            recieveMsg();
+        }
+    }
+
+
+}
+
+// Serial Implementation 
+void STMComm::sendMsg(std::string outMsg) {
+    // Send everything in outMsg through serial port
+    //ROS_INFO("attempting send");
+    stm_board.write(outMsg);
+    ROS_INFO("Sent via serial: %s", outMsg.c_str());
+}
+
+void STMComm::recieveMsg() {
+
+    std::string next_char = "";
+    std::string buffer = "";
+    int timeoutCounter = 0;
+    do {
+        timeoutCounter ++;
+        next_char = stm_board.read();
+        buffer += next_char;
+    //    if(timeoutCounter > 50){
+    //     ROS_INFO("timed out");
+    //     next_char = "Z";
+    //    }
+    } while (next_char != "\n");
+
+     ROS_INFO("Tempurature Buffer: %s", buffer.c_str());
+
+    pubUtility();
+    // // Update parameters based on feedback
+
+
+}
+
+
+void STMComm::pubUtility(){
+
+}
+
+
